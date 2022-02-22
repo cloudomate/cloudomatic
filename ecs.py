163f18f9798bfb45c9189484cb8ac4b808363fff
ysj <masteryodareturns@gmail.com>
2022-02-22 21:35:39 +0400

    initial commit

---
+++ ecs.py
@@ -0,0 +1,145 @@
+# coding=utf-8
+import requests
+import json
+from config import imageid,availability_zone,vpcid,sgid,subnetid,project_id, ecs_endpoint,sshkeyname
+import argparse
+from tokeng42 import *
+import random
+import string
+
+
+#post
+create_ecs_url="https://"+ecs_endpoint+"/v1/"+project_id+"/cloudservers"
+
+#get
+query_ecs_list_url="https://"+ecs_endpoint+"/v1/"+project_id+'/cloudservers/detail'
+
+#post
+del_ecs_url="https://"+ecs_endpoint+"/v1/"+project_id+'/cloudservers/delete'
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+ecs_config = {
+    "server": {
+        "availability_zone":availability_zone,
+        "name": "rancher-test", 
+        "imageRef": imageid, 
+        "root_volume": {
+            "volumetype": "SAS"
+        }, 
+        "data_volumes": [ 
+            {
+                "volumetype": "SAS", 
+                "size": 50
+            }
+        ], 
+        "flavorRef": "s6.large.2", 
+        "vpcid": vpcid, 
+        "security_groups": [
+            {
+                "id": sgid
+            }
+        ], 
+        "nics": [
+            {
+                "subnet_id": subnetid
+            }
+        ], 
+        "key_name": sshkeyname, 
+        "count": 1, 
+        "server_tags": [
+            {
+                "key": "key1",
+                "value": "value1"
+            }
+        ]
+    }
+}
+
+del_ecs_json  = {
+    "servers": [
+        {
+            "id": ""
+        }
+    ], 
+    "delete_publicip": True, 
+    "delete_volume": True
+   }
+
+
+def query_ecs_list():
+    token = get_token(get_token_post_json)
+    params= {"offset":1,"limit":100}
+    if token:
+        r=requests.get(query_ecs_list_url,params=params,headers={'content-type': 'application/json','X-Auth-Token':token})
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return r.content
+    else:
+        print("token error")
+        return "error"   
+
+def del_ecs(ecs_name):
+    ecs_list = query_ecs_list()
+    id = find_ecs_id(ecs_name,ecs_list)
+    if id != "error":
+        token = return_if_valid_token()
+        del_ecs_json["servers"][0]["id"] = id
+        if token:
+            print("deleting",ecs_name,id)
+            r=requests.post(del_ecs_url,data=json.dumps(del_ecs_json),headers={'content-type': 'application/json','X-Auth-Token':token})
+            print(r.status_code,r.content)
+    else:
+        print("ecs does not exsist")
+
+def find_ecs_id(ecs_name,ecs_list):
+    for ecs in ecs_list["servers"]:
+        if ecs["name"] == ecs_name:
+            id = ecs["id"]
+            return id
+        else:
+            return "error"
+
+def create_ecs(vmname,flavour):
+    token = return_if_valid_token()
+    if token != "error":
+        ecs_config["server"]["name"]=vmname
+        ecs_config['server']['flavorRef']=flavour
+        r=requests.post(create_ecs_url,data=json.dumps(ecs_config),headers={'content-type': 'application/json','X-Auth-Token':token})
+        if r.status_code == 201:
+            print(r.content)
+        print(r.content)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-c','--create', help='Create ECS')
+    parser.add_argument('-f','--flavour', help='ECS flaour', nargs='?', default="s6.large.2")
+    parser.add_argument('-d','--delete', help='Delete ECS')
+    parser.add_argument('-l','--list',help="get ECS list",action='store_true')
+    args = parser.parse_args()
+
+    if (args.create):
+        print('creating vm with name',args.create)
+        create_ecs(args.create,args.flavour)
+    elif (args.delete):
+        print('deleting vm with name',args.delete)
+        del_ecs(args.delete)
+    elif args.list:
+        h = ['Name', 'OS', 'Flavour',"IPv4 Addr"]
+        ecs_list=query_ecs_list()
+        print('{:<60s} {:<40s} {:<20s} {:<40}'.format(*h))
+        for ecs in ecs_list["servers"]:
+            vpcid=list(ecs["addresses"].keys())[0]
+            detail_list=[
+                         ecs['name'],
+                         ecs['metadata']['image_name'],
+                         ecs['flavor']['name'],
+                         ecs["addresses"][vpcid][0]['addr']
+                          ]
+            print('{:<60s} {:<40s} {:<20s} {:<40s}'.format(*detail_list))
---
+++ rds.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+import requests
+import json
+from config import dbname,dbuser,project_id,rds_instance_id
+import sys
+from tokeng42 import get_token
+
+
+#post
+create_db_url='https://rds.ae-ad-1.g42cloud.com/v3/'+project_id+'/instances/'+rds_instance_id+'/database'
+
+#delete
+delete_db_url='https://rds.ae-ad-1.g42cloud.com/v3/'+project_id+'/instances/'+rds_instance_id+'/database/'+dbname
+
+#post
+authorise_db_url='https://rds.ae-ad-1.g42cloud.com/v3/'+project_id+'/instances/'+rds_instance_id+'/db_privilege'
+
+#get 
+query_db_url='https://rds.ae-ad-1.g42cloud.com/v3/'+project_id+'/instances/'+rds_instance_id+'/database/detail?page=1&limit=10'
+
+
+create_rds_json = {
+    "name": dbname,
+    "character_set": "utf8"
+}
+
+
+rds_authorise_json = {
+    "db_name": dbname,
+    "users":[
+        {
+            "name":dbuser,
+            "readonly": False
+        }
+    ]
+}
+
+
+def del_db(db_name):
+    db_list = query_db()
+    resp = if_db_exists(db_list)
+    if resp == 1:
+        token = get_token()
+        if token:
+            print("deleting",db_name)
+            r=requests.delete(delete_db_url,headers={'content-type': 'application/json','X-Auth-Token':token})
+            print(r.status_code,r.content)
+        else:
+            print("token error")
+    else :
+        print("db does not exist")
+
+
+def query_db():
+    token = get_token()
+    params= {"offset":1,"limit":10}
+    if token:
+        r=requests.get(query_db_url,params=params,headers={'content-type': 'application/json','X-Auth-Token':token})
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return "error"
+    else:
+        return "token error"
+
+def if_db_exists(dblist):
+    for rds in dblist["databases"]:
+        if rds["name"]==dbname:
+            return 1
+        else:
+            return 0
+
+def authorise_db():
+    token = get_token()
+    if token:
+        r=requests.post(authorise_db_url,data=json.dumps(rds_authorise_json),headers={'content-type': 'application/json','X-Auth-Token':token})
+        if r.json()["resp"] == "successful":
+            print("authorization done !")
+            print(r.content)
+
+
+def create_db(dbname=dbname):
+    db_list = query_db()
+    resp = if_db_exists(db_list)
+    if resp == 1:
+        del_db(dbname)
+    token = get_token()
+    if token:
+        r=requests.post(create_db_url,data=json.dumps(create_rds_json),headers={'content-type': 'application/json','X-Auth-Token':token})
+        if r.json()["resp"] == "successful":
+            print(r.content)
+            print("created db now authorizing...",dbuser)
+            authorise_db()
+
+if __name__=='__main__':
+    if len(sys.argv) < 2:
+        print("argument missing")
+        exit 
+    elif len(sys.argv) == 2:
+        action = sys.argv[1]
+        if action == "create":
+            create_db()
+        elif action == "delete":
+            del_db(dbname)
+        
