0b98720b53dab48a711c988627e7e1eea5b53f1a
ysj <masteryodareturns@gmail.com>
2022-02-26 21:34:01 +0400

    update ip fetch

--- rancher.py
+++
@@ -1,4 +0,0 @@
-from ecs import create_ecs, query_ip_from_ecs
-from rds import create_db
-import os
-
--- test.sh
+++
@@ -1,3 +0,0 @@
-host1="rancher-bastion"
-ipaddr=$(python3 ecs.py -a $host1)
-echo "ip addr is : "$ipaddr
